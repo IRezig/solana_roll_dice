0d844560b361bd8e51eb1aa5b886e755fe107388
Twisted <julien.brunet.92@gmail.com>
2022-12-17 00:44:54 +0100

    added husky & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	extends: [
+	  "eslint:recommended",
+	  "plugin:@typescript-eslint/recommended",
+	  "prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	root: true,
+	ignorePatterns: [".eslintrc.js", "config-overrides.js", "build"],
+	rules: {
+	  "@typescript-eslint/no-unused-vars": [
+		"warn",
+		{
+		  argsIgnorePattern: "^_",
+		  varsIgnorePattern: "^_",
+		},
+	  ],
+	  "@typescript-eslint/no-explicit-any": "off",
+	  "@typescript-eslint/no-var-requires": "off",
+	},
+  };
+  
