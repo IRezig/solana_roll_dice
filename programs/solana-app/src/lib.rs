3df06ab1bc2b6e7d40274dbf5b15f73ce23b12dd
ibtissemr <ibtissem@abyssale.com>
2022-12-16 01:34:00 +0100

    program

--- programs/solana-app/src/lib.rs
+++ programs/solana-app/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
+mod init;
 use anchor_lang::solana_program::system_program;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
+pub use crate::init::initializer::*;
 
 declare_id!("AHpwncxAnUsYngmKQajpgrRjZP3Gz4ysZiLQqjWZoBWK");
 
@@ -8,69 +10,13 @@ declare_id!("AHpwncxAnUsYngmKQajpgrRjZP3Gz4ysZiLQqjWZoBWK");
 pub mod solana_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let player_state = &mut ctx.accounts.player_state;
-        let current_round = &mut ctx.accounts.current_round;
-        
-        current_round.id = 2;
-        player_state.bet = 0;
-        player_state.bump = *ctx.bumps.get("player_state").unwrap();
+    pub fn init_current_round(ctx: Context<InitializeAppState>) -> Result<()> {
+        init_current_round(ctx);
+        Ok(())
+    }
+
+    pub fn init_player(ctx: Context<InitializePlayer>) -> Result<()> {
+        init_player(ctx);
         Ok(())
     }
 }
-
-#[account]
-pub struct PlayerState {
-    pub bet: u16,
-    pub bump: u8,
-}
-
-#[account]
-pub struct CurrentRound {
-    pub id: u16,
-    pub bump: u8,
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(
-        init,
-        payer = player, 
-        space = PlayerState::LEN, 
-        seeds = [b"player_state".as_ref(), player.key().as_ref()],
-        bump,
-    )]
-    pub player_state: Account<'info, PlayerState>,
-
-    #[account(
-        init,
-        payer = player, 
-        space = CurrentRound::LEN, 
-        seeds = [b"current_round".as_ref()],
-        bump,
-    )]
-    pub current_round: Account<'info, CurrentRound>,
-
-    #[account(mut)]
-    pub player: Signer<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info, System>,
-}
-
-const DISCRIMINATOR_LENGTH: usize = 8;
-const PUBLIC_KEY_LENGTH: usize = 32;
-const TIMESTAMP_LENGTH: usize = 8;
-
-impl PlayerState {
-    const LEN: usize = DISCRIMINATOR_LENGTH
-        + 2 // bet
-        + 1 // bump
-        ;
-} 
-
-impl CurrentRound {
-    const LEN: usize = DISCRIMINATOR_LENGTH
-        + 2 // id
-        + 1 // bump
-        ;
-} 
--- tests/solana-app.ts
+++ tests/solana-app.ts
@@ -2,7 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaApp } from "../target/types/solana_app";
 import { PublicKey } from '@solana/web3.js';
-import { expect } from "chai";
+import { assert, expect } from "chai";
 
 describe("solana-app", () => {
   // Configure the client to use the local cluster.
@@ -19,28 +19,75 @@ describe("solana-app", () => {
     return pda
   }
 
-  it('can init accounts', async () => {
+  it('can init app sate', async () => {
+    // const signature = await program.provider.connection.requestAirdrop(player.publicKey, 1000000000);
+    // await program.provider.connection.confirmTransaction(signature);
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+    const pubKey = provider.wallet.publicKey;
+    const c = getPDA("current_round")
+
+    await program.methods
+    .initCurrentRound()
+    .accounts({
+      player: pubKey,
+      currentRound: c,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .rpc()
+
+    let success = false
+    try {
+      await program.methods
+        .initCurrentRound()
+        .accounts({
+          player: pubKey,
+          currentRound: c,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc()
+        success = true
+
+      } catch(e){}
+    expect(success).to.be.false;
+
+    const current = await program.account.currentRound.fetch(c);
+    expect(current.id).equal(2)
+  });
+
+  it('can init player', async () => {
     // const signature = await program.provider.connection.requestAirdrop(player.publicKey, 1000000000);
     // await program.provider.connection.confirmTransaction(signature);
     const provider = anchor.AnchorProvider.env();
     anchor.setProvider(provider);
     const pubKey = provider.wallet.publicKey;
     const p = getPDA("player_state", pubKey)
-    const c = getPDA("current_round")
 
     await program.methods
-      .initialize()
-      .accounts({
-        player: pubKey,
-        playerState: p,
-        currentRound: c,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .rpc()
+    .initPlayer()
+    .accounts({
+      player: pubKey,
+      playerState: p,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .rpc()
 
+    let success = false
+    try {
+      await program.methods
+        .initPlayer()
+        .accounts({
+          player: pubKey,
+          playerState: p,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc()
+        success = true
+      } catch(e){}
+    
+    expect(success).to.be.false;
     const playerAccount = await program.account.playerState.fetch(p);
     expect(playerAccount.bet).equal(0)
-    const current = await program.account.currentRound.fetch(c);
-    expect(current.id).equal(2)
   });
+
 });
