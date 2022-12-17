d26aed60fe7cd805a2ac1e93bad3d884a8673fa4
ibtissemr <ibtissem@abyssale.com>
2022-12-17 01:38:51 +0100

    test

--- tests/solana-app.ts
+++ tests/solana-app.ts
@@ -1,30 +1,30 @@
 import { expect } from "chai";
-import {
-  getCurrentRound,
-  getPlayerState,
-  getWalletKey,
-  initAppState,
-  initPlayer,
-  getLastRound,
-  getStats,
-} from "./utils";
+import { getCurrentRound, getPlayerState, getWalletKey, initAppState, initPlayer, getLastRound, getStats  } from "./utils";
 
 describe("solana-app", () => {
-  const besma = getWalletKey();
 
-  it("can init app sate", async () => {
-    await initAppState();
-    expect(initAppState()).to.throw;
+	const besma = getWalletKey();
 
-    const current = await getCurrentRound();
-    expect(current.id).equal(2);
-  });
+	it('can init app state', async () => {
+		await initAppState()
+		expect(initAppState()).to.throw;
 
-  it("can init player", async () => {
-    await initPlayer(besma);
-    expect(initPlayer(besma)).to.throw;
+		const current = await getCurrentRound();
+		expect(current.id).equal(1)
 
-    const player = await getPlayerState(besma);
-    expect(player.bet).equal(0);
-  });
+		const last = await getLastRound();
+		expect(last.benefits).equal(0)
+
+		const stats = await getStats();
+		expect(stats.totalRolls).equal(0)
+	});
+
+	it('can init player', async () => {
+		await initPlayer(besma)
+		expect(initPlayer(besma)).to.throw
+
+		const player = await getPlayerState(besma);
+		expect(player.lastClaimedRound).equal(0)
+	});
+
 });
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,15 +1,15 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaApp } from "../target/types/solana_app";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey } from '@solana/web3.js';
 
 /**
  * Seeds
  */
-const CURRENT_ROUND_SEED = "current_round";
-const LAST_ROUND_SEED = "last_round";
-const STATS_SEED = "stats";
-const PLAYER_STATE_SEED = "player_state";
+const CURRENT_ROUND_SEED = "current_round"
+const LAST_ROUND_SEED = "last_round"
+const STATS_SEED = "stats"
+const PLAYER_STATE_SEED = "player_state"
 
 /**
  * Basic setup
@@ -22,58 +22,43 @@ anchor.setProvider(provider);
  * Helpers
  */
 export const addressForSeed = (seed: string, pubKey?: PublicKey) => {
-  const seeds = [anchor.utils.bytes.utf8.encode(seed)];
-  if (pubKey) {
-    seeds.push(pubKey.toBuffer());
-  }
-  const [pda, _] = PublicKey.findProgramAddressSync(seeds, program.programId);
-  return pda;
-};
-export const getCurrentRound = async () =>
-  await program.account.currentRound.fetch(addressForSeed(CURRENT_ROUND_SEED));
-export const getLastRound = async () =>
-  await program.account.lastRound.fetch(addressForSeed(LAST_ROUND_SEED));
-export const getStats = async () =>
-  await program.account.stats.fetch(addressForSeed(STATS_SEED));
-export const getPlayerState = async (pubKey: PublicKey) =>
-  await program.account.playerState.fetch(
-    addressForSeed(PLAYER_STATE_SEED, pubKey)
-  );
-export const getWalletKey = () => provider.wallet.publicKey;
+	const seeds = [anchor.utils.bytes.utf8.encode(seed)]
+	if (pubKey) {
+		seeds.push(pubKey.toBuffer())
+	}
+	const [pda, _] = PublicKey.findProgramAddressSync(seeds, program.programId)
+	return pda
+}
+export const getCurrentRound = async () => await program.account.currentRound.fetch(addressForSeed(CURRENT_ROUND_SEED))
+export const getLastRound = async () => await program.account.lastRound.fetch(addressForSeed(LAST_ROUND_SEED))
+export const getStats = async () => await program.account.stats.fetch(addressForSeed(STATS_SEED))
+export const getPlayerState = async (pubKey: PublicKey) => await program.account.playerState.fetch(addressForSeed(PLAYER_STATE_SEED, pubKey))
+export const getWalletKey = () => provider.wallet.publicKey
 
 export const initAppState = async () => {
-  const currentRound = addressForSeed(CURRENT_ROUND_SEED);
-  const lastRound = addressForSeed(LAST_ROUND_SEED);
-  const stats = addressForSeed(STATS_SEED);
+    const currentRound = addressForSeed(CURRENT_ROUND_SEED)
+    const lastRound = addressForSeed(LAST_ROUND_SEED)
+    const stats = addressForSeed(STATS_SEED)
 
-  return await program.methods
-    .initAppState()
-    .accounts({
-      player: provider.wallet.publicKey,
-      currentRound,
-      lastRound,
-      stats,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    })
-    .rpc();
-};
+	return await program.methods.initAppState().accounts({
+		player: provider.wallet.publicKey,
+		currentRound,
+		lastRound,
+		stats,
+		systemProgram: anchor.web3.SystemProgram.programId,
+	}).rpc()
+}
 
 export const initPlayer = async (pubKey: PublicKey) => {
-  const playerState = addressForSeed(PLAYER_STATE_SEED, pubKey);
-  return await program.methods
-    .initPlayer()
-    .accounts({
-      player: provider.wallet.publicKey,
-      playerState,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    })
-    .rpc();
-};
+    const playerState = addressForSeed(PLAYER_STATE_SEED, pubKey)
+	return await program.methods.initPlayer().accounts({
+		player: provider.wallet.publicKey,
+		playerState,
+		systemProgram: anchor.web3.SystemProgram.programId,
+	}).rpc()
+}
 
 export const airdrop = async (pubKey: PublicKey, amount = 1000000000) => {
-  const signature = await program.provider.connection.requestAirdrop(
-    pubKey,
-    amount
-  );
-  await program.provider.connection.confirmTransaction(signature);
-};
+	const signature = await program.provider.connection.requestAirdrop(pubKey, amount);
+	await program.provider.connection.confirmTransaction(signature);
+}
