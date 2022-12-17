d17b54ff117f091641bc42f1ef2e68ea5db1ded2
Twisted <julien.brunet.92@gmail.com>
2022-12-17 03:41:13 +0100

    cleanup

--- tests/solana-app.ts
+++ tests/solana-app.ts
@@ -16,28 +16,6 @@ describe("solana-app", () => {
   const besma = getWalletKey();
   const ju = Keypair.generate();
 
-  // it('can init app state', async () => {
-  // 	await initAppState()
-  // 	expect(initAppState()).to.throw;
-
-  // 	const current = await getCurrentRound();
-  // 	expect(current.id).equal(1)
-
-  // 	const last = await getLastRound();
-  // 	expect(last.benefits).equal(0)
-
-  // 	const stats = await getStats();
-  // 	expect(stats.totalRolls).equal(0)
-  // });
-
-  // it('can init player', async () => {
-  // 	await initPlayer(besma)
-  // 	expect(initPlayer(besma)).to.throw
-
-  // 	const player = await getPlayerState(besma);
-  // 	expect(player.lastClaimedRound).equal(0)
-  // });
-
   it("can play", async () => {
     await initAppState();
     await initPlayer(besma);
@@ -46,5 +24,14 @@ describe("solana-app", () => {
 
     const current = await getCurrentRound();
     expect(current.benefits).equal(GAME_PRICE);
+
+	const last = await getLastRound();
+	expect(last.benefits).equal(0)
+
+	const stats = await getStats();
+	expect(stats.totalRolls).equal(0)
+
+	const player = await getPlayerState(besma);
+	expect(player.lastClaimedRound).equal(0)
   });
 });
